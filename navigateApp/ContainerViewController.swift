fe93236447445b09b54b2da9783bdac2049f52c8
ponnjinnka <penponnnnjinnka@gmail.com>
2017-03-29 08:37:49 +0900

    add container class

---
+++ navigateApp/ContainerViewController.swift
@@ -0,0 +1,15 @@
+//
+// Created by ryota on 2017/03/29.
+// Copyright (c) 2017 Ryota Kato. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //背景色を黄色にする
+        self.view.backgroundColor = UIColor.yellow
+    }
+}
